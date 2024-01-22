62015bee98baea080d56751d35cc15ba5cc8c871
Vishnu <itsvishnups@gmail.com>
2024-01-22 15:09:54 +0530

    Update login and reg2

--- src/terms.js
+++ src/terms.js
@@ -5,7 +5,7 @@ function terms() {
   return (
     <div>
       <h1>Terms and Conditions</h1>
-      {"Welcome to DigiCapsule, a digital platform dedicated to storing and safeguarding your digital memories. By accessing our service, you agree to be bound by these Terms and Conditions, which govern your use of [Your Website Name] and its services. It is vital to read these terms carefully and ensure you understand them before using our platform."
+      {"Welcome to DigiCapsule, a digital platform dedicated to storing and safeguarding your digital memories. By accessing our service, you agree to be bound by these Terms and Conditions, which govern your use of DigiCapsule and its services. It is vital to read these terms carefully and ensure you understand them before using our platform."
 }</div>
   );
 }
